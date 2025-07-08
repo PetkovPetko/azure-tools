b4376ac4bc9fa4c81f72833d32eb5c401c67e57d
PetkovPetko <pkirilov@david.bg>
2025-07-08 16:29:24 +0300

    Migrate function to the isolated worker model Update nuget packages Add more log events for better analytics

--- AzureNetTools/MissingArchiveCheck.cs
+++ AzureNetTools/MissingArchiveCheck.cs
@@ -4,137 +4,139 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Azure;
 using Azure.Identity;
 using Azure.Storage.Blobs;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
 using Newtonsoft.Json;
 
-namespace AzureNetTools
+namespace AzureNetTools;
+
+public class MissingArchiveCheck
 {
-    public class MissingArchiveCheck
+    [Function(nameof(MissingArchiveCheck))]
+    public async Task RunAsync(
+        [TimerTrigger("0 0 10 * * *")] TimerInfo myTimer,
+        FunctionContext context)
     {
-        [FunctionName("MissingArchiveCheck")]
-        public async Task Run(
-            [TimerTrigger("0 0 10 * * *")] TimerInfo myTimer,
-            ILogger log)
-        {
-            TimeZoneInfo sofiaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("FLE Standard Time");
-            DateTime localTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, sofiaTimeZone);
+        var log = context.GetLogger(nameof(MissingArchiveCheck));
 
-            string message = $"C# Timer trigger function executed at: {localTime}";
-            log.LogInformation(message);
+        TimeZoneInfo sofiaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("FLE Standard Time");
+        DateTime localTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, sofiaTimeZone);
 
-            var yesterdayFileName = $"{localTime.AddDays(-1).ToString("yyyyMMdd")}.tgz";
-            var containerName = Settings.Container;
-            List<Section> sections = new();
+        string message = $"C# Timer trigger function executed at: {localTime}";
+        log.LogInformation(message);
 
-            foreach (var connection in Settings.Connections)
-            { 
-                var defaultCredentials = new DefaultAzureCredential();
-                var blobServiceUri = new Uri(connection);
-                
-                BlobServiceClient blobServiceClient = new BlobServiceClient(blobServiceUri, defaultCredentials);
+        var yesterdayFileName = $"{localTime.AddDays(-1).ToString("yyyyMMdd")}.tgz";
+        var containerName = Settings.Container;
+        List<Section> sections = new();
 
-                CheckIfContainersExists(blobServiceClient, log, containerName);
+        foreach (var connection in Settings.Connections)
+        { 
+            var defaultCredentials = new DefaultAzureCredential();
+            var blobServiceUri = new Uri(connection);
+            
+            BlobServiceClient blobServiceClient = new BlobServiceClient(blobServiceUri, defaultCredentials);
 
-                var exists = DoesArchiveExist(blobServiceClient, containerName, yesterdayFileName, log);
-                if (!exists)
-                {
-                    sections.Add(this.CreateCardSection(yesterdayFileName, containerName, blobServiceClient.AccountName));
-                }
-            }
+            CheckIfContainersExists(blobServiceClient, log, containerName);
 
-            if (sections.Any())
+            var exists = DoesArchiveExist(blobServiceClient, containerName, yesterdayFileName, log);
+            if (!exists)
             {
-                await CreateTeamsCard(sections, localTime);
+                sections.Add(this.CreateCardSection(yesterdayFileName, containerName, blobServiceClient.AccountName));
             }
         }
 
-        private void CheckIfContainersExists(BlobServiceClient blobServiceClient, ILogger log, string container)
+        if (sections.Any())
         {
-            var missingContainer = false;
+            log.LogInformation("Posting to Teams. Sections = {0}", sections.Count);
 
-            var containerExists = blobServiceClient.GetBlobContainers().Any(s => s.Name.Equals(container, StringComparison.OrdinalIgnoreCase));
-            if (!containerExists)
+            await CreateTeamsCard(sections, localTime, log);
+        }
+    }
+
+    private void CheckIfContainersExists(BlobServiceClient blobServiceClient, ILogger log, string container)
+    {
+        var missingContainer = false;
+
+        var containerExists = blobServiceClient.GetBlobContainers().Any(s => s.Name.Equals(container, StringComparison.OrdinalIgnoreCase));
+        if (!containerExists)
+        {
+            missingContainer = true;
+            log.LogError($"Container {container} does not exist for storage: {blobServiceClient.AccountName}");
+        }
+
+        if (missingContainer)
+        {
+            throw new Exception("Missing containers, check logs for more information");
+        }
+    }
+
+    private bool DoesArchiveExist(BlobServiceClient blobServiceClient, string containerName, string fileName, ILogger log)
+    {
+        var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        var blobClient = containerClient.GetBlobClient(fileName);
+        return blobClient.Exists();
+    }
+
+    private Section CreateCardSection(string yesterdayFileName, string container, string storageAccountName)
+    {
+        var facts = SetFacts(yesterdayFileName, container, storageAccountName);
+
+        var section = new Section
+        {
+            StartGroup = true,
+            Title = "Storage information",
+            Facts = facts
+        };
+
+        return section;
+    }
+
+    private List<Fact> SetFacts(string yesterdayFileName, string container, string storageAccountName)
+    {
+        var facts = new List<Fact>
+        {
+            new Fact
             {
-                missingContainer = true;
-                log.LogError($"Container {container} does not exist for storage: {blobServiceClient.AccountName}");
+                Name = "Storage account",
+                Value = storageAccountName
+            },
+            new Fact
+            {
+                Name = "Container",
+                Value = container
+            },
+            new Fact
+            {
+                Name = "Expected file",
+                Value = yesterdayFileName
             }
+        };
 
-            if (missingContainer)
-            {
-                throw new Exception("Missing containers, check logs for more information");
-            }
-        }
+        return facts;
+    }
 
-        private bool DoesArchiveExist(BlobServiceClient blobServiceClient, string containerName, string fileName, ILogger log)
+    private async Task CreateTeamsCard(List<Section> sections, DateTime localTime, ILogger log)
+    {
+        var card = new TeamsCard
         {
-            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-            var blobClient = containerClient.GetBlobClient(fileName);
-            return blobClient.Exists();
-        }
+            Type = "MessageCard",
+            Context = "http://schema.org/extensions",
+            ThemeColor = "00FF00",
+            Title = "Missing Azure backup",
+            Text = $"*Missing backup for {localTime.AddDays(-1).ToString("dd.MM.yyyy")}*",
+            Sections = sections
+        };
 
-        private Section CreateCardSection(string yesterdayFileName, string container, string storageAccountName)
-        {
-            var facts = SetFacts(yesterdayFileName, container, storageAccountName);
+        var jsonTemplate = JsonConvert.SerializeObject(card);
 
-            var section = new Section
-            {
-                StartGroup = true,
-                Title = "Storage information",
-                Facts = facts
-            };
+        var client = new HttpClient();
+        var content = new StringContent(jsonTemplate, Encoding.UTF8, "application/json");
 
-            return section;
-        }
+        var resp = await client.PostAsync(Settings.WebhookUrl, content);
 
-        private List<Fact> SetFacts(string yesterdayFileName, string container, string storageAccountName)
-        {
-            var facts = new List<Fact>
-            {
-                new Fact
-                {
-                    Name = "Storage account",
-                    Value = storageAccountName
-                },
-                new Fact
-                {
-                    Name = "Container",
-                    Value = container
-                },
-                new Fact
-                {
-                    Name = "Expected file",
-                    Value = yesterdayFileName
-                }
-            };
-
-            return facts;
-        }
-
-        private async Task CreateTeamsCard(List<Section> sections, DateTime localTime)
-        {
-            var card = new TeamsCard
-            {
-                Type = "MessageCard",
-                Context = "http://schema.org/extensions",
-                ThemeColor = "00FF00",
-                Title = "Missing Azure backup",
-                Text = $"*Missing backup for {localTime.AddDays(-1).ToString("dd.MM.yyyy")}*",
-                Sections = sections
-            };
-
-            var jsonTemplate = JsonConvert.SerializeObject(card);
-
-            var client = new HttpClient();
-            var content = new StringContent(jsonTemplate, Encoding.UTF8, "application/json");
-
-            await client.PostAsync(Settings.WebhookUrl, content);
-        }
+        log.LogInformation("Teams status: {0}", resp.StatusCode);
+        resp.EnsureSuccessStatusCode();
     }
 }
---
+++ AzureNetTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        // Ако искаш DI (dependency injection), го правиш тук
+    })
+    .Build();
+
+host.Run();
